e979a04dc8d4d8f4fc9b6cd24047ad646db41b27
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-05-17 01:00:11 +0200

    Selecting Buttons(Javascript file)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 //Selecting Carousel Slide and Imgs
 const carouselSlide = document.querySelector(".carousel-slide");
-const carouselImages = document.querySelector(".carousel-slide img")
+const carouselImages = document.querySelector(".carousel-slide img");
+// Buttons Selction
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
