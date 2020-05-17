a871c78b05ec3b8e97e075fb15088f0246f36af7
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-05-18 01:42:32 +0200

    MakingCounter-CreatingSizeVarAsWidthOfThePic)

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const carouselImages = document.querySelector(".carousel-slide img");
 // Buttons Selction
 const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
+// Making Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
