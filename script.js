81ca2af981510cf1ac6fc248a3839b33fa78a456
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-05-18 19:38:43 +0200

    Adding Button Listeners (NxtButtn testing)

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const nextBtn = document.querySelector("#nextBtn");
 // Making Counter
 let counter = 1;
 const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = "translateX(" + (-size * counter) + "px)";
+
+// Buttons Listeners
+nextBtn.addEventListener("click",()=>{
+    carouselSlide.style.transition = "trasform 0.4s ease-in-out";
+    counter++;
+    console.log(counter)
+})
